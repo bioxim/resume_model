9dc788ed578dd54f416fd312f790b8b974f21905
Xime Camino <ximecamino@gmail.com>
2022-09-05 12:20:57 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* ********** Menu ********** */
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn"),
+      $menu = d.querySelector(".menu");
+  
+    $btnMenu.addEventListener("click", (e) => {
+      $btnMenu.firstElementChild.classList.toggle("none");
+      $btnMenu.lastElementChild.classList.toggle("none");
+      $menu.classList.toggle("is-active");
+    });
+  
+    d.addEventListener("click", (e) => {
+      if (!e.target.matches(".menu a")) return false;
+  
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+    });
+  })(document);
+
+  /* ********** Animated effects ********** */
+  gsap.to(".subtitle", {
+    duration: 3, 
+    x: 20,
+    repeat: -1
+  })
+
+
+  
